81ff004fc012982b0ee2b789673d174d7a9ea48f
dxl <2630537356@qq.com>
2023-05-31 13:32:47 +0800

    第一次提交

---
+++ src/test/java/com/dxl/javasynthesis/JavaSynthesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxl.javasynthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSynthesisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
